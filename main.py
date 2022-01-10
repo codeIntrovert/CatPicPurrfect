a2e235ecd50eb1ac3397c56744198b5a2bdade8f
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-10 17:38:04 +0530

    added tweet count, tweet system

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import tweepy
 import time
 from key import *
+
+initial_twittes = 0
 def tweeter():
-    #access verfication
     auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
     auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 
     api = tweepy.API(auth, wait_on_rate_limit = True)
 
-    search = '#100daysofcode'
+    search = '#codenewbie'
     nrTweets = 500
     print("List loaded starting retwating")
     for tweet in tweepy.Cursor(api.search_tweets, search).items(nrTweets):
         try: 
             tweet.retweet()
-            print("retwat will check again in 60 sec ")
-            time.sleep(60)
+            print("retwitter will check again in 90 sec ")
+            time.sleep(90)
+            final_twittes = initial_twittes+1
+            if final_twittes%100 == 0:
+                tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS, congrats everyone!{HASTAGS}")
         except:
-            print("ERROR:: retwat will restart in 60 secs")
+            print("ERROR:: retwitter will restart in 60 secs")
             time.sleep(60)
             tweeter()
 tweeter()
