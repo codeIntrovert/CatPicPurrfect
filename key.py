18cb833eb14deaaeea0a30699650ff3d36e62875
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-10 17:36:17 +0530

    added hastags

--- key.py
+++ key.py
@@ -3,3 +3,5 @@ API_KEY ="4FXGUFKOz9qgurNXqvbPdIYQw"
 
 ACCESS_TOKEN ="1480190505812049922-A8zYbjgP9UmyzMqM7HhA7bmU80a2KO"
 ACCESS_SECRET ="fpeIOzgZETGmY0RqQvDhhG1EPkRkgDtx94uOZ4JBiFWc7"
+
+HASTAGS = "#Coding #Congrats #100daysofcode #python #WomenWhoCode #codeNewbies @elonmusk @python"
