611ce4d88b565dc7ca95907d08d22afbfbacac37
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-11 00:11:22 +0530

    MAJOR UPDATE V1.0
    FIXED PERFOMANCE
    FIXED BAAR BAAR API LOGIN ISSUE
    FIXED TWEET SPEED
    FIXED ERROR EXCEPTION ISSUE
    FIXED ANNOUNCEMENT FEATURE

--- main.py
+++ main.py
@@ -2,26 +2,32 @@ import tweepy
 import time
 from key import *
 
-initial_twittes = 0
+'''variable table'''
+REMAP_TIME = 90
+ERROR_TIME = 5
+auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
+API = tweepy.API(auth, wait_on_rate_limit = True)
+
 def tweeter():
-    auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
-    auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
-
-    api = tweepy.API(auth, wait_on_rate_limit = True)
-
     search = '#codenewbie'
-    nrTweets = 500
-    print("List loaded starting retwating")
-    for tweet in tweepy.Cursor(api.search_tweets, search).items(nrTweets):
+    nrTweets = 50
+    count = 0
+    for tweet in tweepy.Cursor(API.search_tweets, search).items(nrTweets):
         try: 
             tweet.retweet()
-            print("retwitter will check again in 90 sec ")
-            time.sleep(90)
-            final_twittes = initial_twittes+1
-            if final_twittes%100 == 0:
-                tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS, congrats everyone!{HASTAGS}")
-        except:
-            print("ERROR:: retwitter will restart in 60 secs")
-            time.sleep(60)
+            print(f"retwitter will check again in {REMAP_TIME} sec ")
+            count +=1
+            if count%100==0:
+                final_twittes = int(count+1000)
+                tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS! {HASTAGS}")
+                print("announced")
+            time.sleep(REMAP_TIME)
+
+        except Exception as e:
+            print(f"ERROR:: retwitter will restart in {ERROR_TIME} secs {e}")
+            time.sleep(ERROR_TIME)
             tweeter()
 tweeter()
+#code finalized, updated on 11/jan/2022
+#@HasanBOT_PY, signed in with google public id, MYNAMEIS
