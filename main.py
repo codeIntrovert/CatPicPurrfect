c637fc76f8414aca82bb075a5ce4a4ec4866721e
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-11 00:23:53 +0530

    minor patch

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 API = tweepy.API(auth, wait_on_rate_limit = True)
 
 def tweeter():
-    search = '#codenewbie'
+    search = '#100DaysOfCode'
     nrTweets = 50
     count = 0
     for tweet in tweepy.Cursor(API.search_tweets, search).items(nrTweets):
