bd24cffea840697e56e2db1416df9de665493d4f
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-11 22:35:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ def tweeter():
     search = '#codenewbie'
     nrTweets = 50
     count = 0
+
     for tweet in tweepy.Cursor(API.search_tweets, search).items(nrTweets):
+
         try: 
             tweet.retweet()
             print(f"retwitter will check again in {REMAP_TIME} sec ")
-            count +=1
-            
             time.sleep(REMAP_TIME)
-
         except Exception as e:
             print(f"ERROR:: retwitter will restart in {ERROR_TIME} secs {e}")
             time.sleep(ERROR_TIME)
             tweeter()
+
+        count +=1
         if count%100==0:
             final_twittes = int(count+1000)
             tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS! {HASTAGS}")
@@ -32,4 +33,4 @@ def tweeter():
 tweeter()
 #code finalized, updated on 11/jan/2022
 #@HasanBOT_PY, signed in with google public id, MYNAMEIS
-#contiued minor bug fixes
+#continued minor bug fixes
