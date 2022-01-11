57e96a8781dbaccf50e5faa9228a550643089b1a
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-11 22:32:51 +0530

    minor bug fixs
    fixed counter, auonncement

--- main.py
+++ main.py
@@ -10,24 +10,26 @@ auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 API = tweepy.API(auth, wait_on_rate_limit = True)
 
 def tweeter():
-    search = '#100DaysOfCode'
+    search = '#codenewbie'
     nrTweets = 50
     count = 0
     for tweet in tweepy.Cursor(API.search_tweets, search).items(nrTweets):
         try: 
             tweet.retweet()
-            print(f"retwitter will check again in {REMAP_TIME} sec{count}")
+            print(f"retwitter will check again in {REMAP_TIME} sec ")
             count +=1
-            if count%100==0:
-                final_twittes = int(count+1000)
-                tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS! {HASTAGS}")
-                print("announced")
+            
             time.sleep(REMAP_TIME)
 
         except Exception as e:
             print(f"ERROR:: retwitter will restart in {ERROR_TIME} secs {e}")
             time.sleep(ERROR_TIME)
             tweeter()
+        if count%100==0:
+            final_twittes = int(count+1000)
+            tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS! {HASTAGS}")
+            print("announced")
 tweeter()
 #code finalized, updated on 11/jan/2022
 #@HasanBOT_PY, signed in with google public id, MYNAMEIS
+#contiued minor bug fixes
