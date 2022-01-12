76288e618b9f1e4b8819fdd75d5b7fb8021467ae
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-12 21:33:21 +0530

    finally counter fixed

--- main.py
+++ main.py
@@ -8,29 +8,32 @@ ERROR_TIME = 5
 auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 API = tweepy.API(auth, wait_on_rate_limit = True)
-COUNT = int(0)
+COUNT = int(1)
+
 def tweeter():
     global COUNT
     search = '#100DaysOfCode'
-    nrTweets = 50
+    nrTweets = 30
 
     for tweet in tweepy.Cursor(API.search_tweets, search).items(nrTweets):
+        if COUNT%100==0:
+            final_twittes = int(COUNT+2500)
+            API.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS! \n {HASTAGS}")
+            print("announced")
 
         try: 
             tweet.retweet()
-            print(f"retwitter will check again in {REMAP_TIME} sec {COUNT}")
+            print(f"next post in {REMAP_TIME} sec; POSTS = {COUNT}")
             time.sleep(REMAP_TIME)
             COUNT +=1
         except Exception as e:
-            print(f"ERROR:: retwitter will restart in {ERROR_TIME} secs {e}")
+            print(f"ERROR: restart in {ERROR_TIME} secs; POST = {COUNT} {e}")
             time.sleep(ERROR_TIME)
+            COUNT +=1
             tweeter()
 
 
-        if COUNT%100==0:
-            final_twittes = int(COUNT+2000)
-            tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS! {HASTAGS}")
-            print("announced")
+ 
 tweeter()
 #code finalized, updated on 11/jan/2022
 #@HasanBOT_PY, signed in with google public id, MYNAMEIS
