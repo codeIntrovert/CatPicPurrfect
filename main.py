58f00fe3ed5fcc0c103e2299f7f88ac0734c30cd
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-12 12:49:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ ERROR_TIME = 5
 auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 API = tweepy.API(auth, wait_on_rate_limit = True)
-count = 0
+COUNT = int(0)
 def tweeter():
     search = '#100DaysOfCode'
     nrTweets = 50
@@ -19,14 +19,15 @@ def tweeter():
             tweet.retweet()
             print(f"retwitter will check again in {REMAP_TIME} sec {count}")
             time.sleep(REMAP_TIME)
+            COUNT +=1
         except Exception as e:
             print(f"ERROR:: retwitter will restart in {ERROR_TIME} secs {e}")
             time.sleep(ERROR_TIME)
             tweeter()
 
-        count +=1
-        if count%100==0:
-            final_twittes = int(count+1000)
+
+        if COUNT%100==0:
+            final_twittes = int(COUNT+2000)
             tweet.update_status(f"WE HAVE OFFICIALLY RETWEETED {final_twittes} TWEETS! {HASTAGS}")
             print("announced")
 tweeter()
