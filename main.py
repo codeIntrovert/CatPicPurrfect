0bfa93b651c6e8e8ca0818ac6eedfc661549cb27
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2022-01-19 19:40:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import time
 from key import *
 
 '''variable table'''
-REMAP_TIME = 65
+REMAP_TIME = 100
 ERROR_TIME = 5
 auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 API = tweepy.API(auth, wait_on_rate_limit = True)
 COUNT = int(1)
 POST_ERROR = int(1)
-CALIBRATE = 9300
+CALIBRATE = 9600
 
 def tweeter():
     global COUNT
