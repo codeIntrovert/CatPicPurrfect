ebbb6e59f95f57021306293af141de2fdd89a4df
Hasan (aka introvertCoder) <46672438+Hasan2325dev@users.noreply.github.com>
2022-02-28 20:28:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from key import *
 
 '''variable table'''
 
-REMAP_TIME = 10
+REMAP_TIME = 100
 ERROR_TIME = 5
 auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 API = tweepy.API(auth, wait_on_rate_limit = True)
 COUNT = int(1)
 POST_ERROR = int(1)
-CALIBRATE = 1000
+
 s = open("logFile.txt","a")
 s.write(f"\n\n###___STARTED___###\n\n")
 s.close()
@@ -39,19 +39,6 @@ def tweeter():
                 print(f"ERROR: restart in {ERROR_TIME} secs; POST = {COUNT} ; ERROR = {POST_ERROR} {e}")
                 time.sleep(ERROR_TIME)
                 tweeter()
-
-            
-            
-    
-
-
-   
-
-         
-
-
-
- 
 tweeter()
 #code finalized, updated on 11/jan/2022
 #@HasanBOT_PY, signed in with google public id, MYNAMEIS
