4eecf260406b45fde774a43857dcc72c47835117
Hasan (aka introvertCoder) <46672438+Hasan2325dev@users.noreply.github.com>
2022-02-28 20:59:29 +0530

    fix

--- writer.py
+++ writer.py
@@ -1,13 +1,10 @@
 from github import Github
 
 def filewrite(x):
-    r = open("logFile.txt","a")
-    r.write(x)
-    r.close()
-    
     github = Github('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')
     repository = github.get_user().get_repo('twat')
     # path in the repository
+    content = x
     filename = 'logFile.txt'
-    f = repository.create_file(filename, "create_file via PyGithub")
+    f = repository.create_file(filename, "create_file via PyGithub",content)
 
