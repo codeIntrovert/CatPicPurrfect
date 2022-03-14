61135c2cdf71ee0aad6136c46de58c11255199f7
Hasan (aka introvertCoder) <46672438+Hasan2325dev@users.noreply.github.com>
2022-03-14 10:47:11 +0530

    perfomance update

--- main.py
+++ main.py
@@ -23,58 +23,47 @@ def qoutesAPI():
         author = thoughtJSON['author']
 
         statement = (f"{quote} - {author} #100daysOfCode #python")
-        print(statement)
         API.update_status(statement)
     except Exception as t:
-        print(f"{t}\n\nproblem with fetching thought api")
+        print(f"{t}\nproblem with fetching thought api")
 
 def tweeter():
     global fetchapi
     global COUNT
     global POST_ERROR
     global FAKE
-    tag = ["#100DaysOfCode"]
-    nrTweets = 200
+    tag = "#100DaysOfCode"
+    nrTweets = 100
     
     for status in tweepy.Cursor(API.search_tweets, tag,tweet_mode="extended").items(nrTweets):
         if "dm" in status.full_text.lower() : # dm
-            print("FAKE! DM \n")
+            print("FAKE! DM")
             FAKE+=1
         elif "essay" in status.full_text.lower(): # /
-            print("FAKE! Essay \n")
-            FAKE+=1
-        elif ".ly" in status.full_text.lower(): #courses
-            print("FAKE! .ly\n")
             FAKE+=1
         elif "free" in status.full_text.lower(): #free
-            print("FAKE! FREE\n")
             FAKE+=1
         elif "know more" in status.full_text.lower(): #free
-            print("FAKE! KNOW MORE\n")
-            FAKE+=1
-        elif ".com" in status.full_text.lower(): #LINK
-            print("FAKE! .COM \n")
             FAKE+=1
         elif "whatsapp" in status.full_text.lower(): #Whatsapp
-            print("FAKE! WHATSAPP \n")
             FAKE+=1
 
         else:
             try: 
                 status.retweet()
-                #status.favorite()
                 COUNT +=1
                 print(f"next post in {REMAP_TIME} sec; POSTS = {COUNT}; ERROR = {POST_ERROR}; FAKE = {FAKE} ")
                 time.sleep(REMAP_TIME)
+                if COUNT%160 == 0:
+                    qoutesAPI()
 
             except Exception as e:
                 POST_ERROR +=1
                 print(f"ERROR: restart in {ERROR_TIME} secs; POST = {COUNT}; ERROR = {POST_ERROR}; FAKE = {FAKE} {e}")
                 time.sleep(ERROR_TIME)
                 tweeter()
-            finally:
-                if COUNT%160 == 0:
-                    qoutesAPI()
+
+
 
 tweeter()
 #code finalized, updated on 12/march/2022
