f6628dace028f1981864839b31ccd6ff84f0591d
Hasan (aka introvertCoder) <46672438+Hasan2325dev@users.noreply.github.com>
2022-03-21 11:29:52 +0530

    up

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def tweeter():
                 COUNT +=1
                 print(f"next post in {REMAP_TIME} sec; POSTS = {COUNT}; ERROR = {POST_ERROR}; FAKE = {FAKE} ")
                 time.sleep(REMAP_TIME)
-                if COUNT%80 == 0:
+                if COUNT%180 == 0:
                     qoutesAPI()
 
             except Exception as e:
