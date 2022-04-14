4c6475d43b6cd52684680d88bab18c1c48b4abf0
Hasan (aka introvertCoder) <46672438+Hasan2325dev@users.noreply.github.com>
2022-04-14 11:16:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from key import *
 
 
 REMAP_TIME = 125
-ERROR_TIME = 5
+ERROR_TIME = 10
 auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 API = tweepy.API(auth, wait_on_rate_limit = True)
@@ -32,7 +32,7 @@ def tweeter():
     global COUNT
     global POST_ERROR
     global FAKE
-    tag = "cat"
+    tag = "#catsoftwitter"
     nrTweets = 100
     
     for status in tweepy.Cursor(API.search_tweets, tag,tweet_mode="extended",lang="en").items(nrTweets):
